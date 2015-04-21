22b4cb0d3002643b5246827efb936f9abc5be8b3
contaylo <Contaylo@mymail.mines.edu>
2015-04-21 12:01:31 -0600

    Half of load config files made

--- src/mathGame/BadConfigFormatException.java
+++ src/mathGame/BadConfigFormatException.java
@@ -1,5 +1,12 @@
 package mathGame;
 
 public class BadConfigFormatException extends Exception {
-
+	
+	public BadConfigFormatException(String error){
+		super(error);
+	}
+	
+	public String toString(){
+		return getMessage();
+	}
 }
